ee6ef7eea8b2d103c6afc351a5a60d9ae8cde73d
esuarez <eagle.suarez@gmail.com>
2011-10-17 17:56:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noteman::Application.config.secret_token = '6b55fb95acda7b0f959bf0da4f6e5beaf1375407857d0c86823fe9916c958fa9a106d3c0259b3122cd76e5ceb629049f421c6925914f45ddf6ee6ef3e12910fb'
